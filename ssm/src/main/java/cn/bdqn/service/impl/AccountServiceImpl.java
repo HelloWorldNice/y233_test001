db34291717528dc39a1aa4ff83c32bb88ce8c972
zhangzhao_best <=>
2020-09-14 16:32:26 +0800

    提交项目

---
+++ ssm/src/main/java/cn/bdqn/service/impl/AccountServiceImpl.java
@@ -0,0 +1,26 @@
+package cn.bdqn.service.impl;
+
+import cn.bdqn.domain.Account;
+import cn.bdqn.mapper.AccountMapper;
+import cn.bdqn.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Override
+    public List<Account> queryAll() {
+        return accountMapper.selectAll();
+    }
+
+    @Override
+    public void save(Account account) {
+
+    }
+}
